f52236f9d4f7e77998617264aa777a49af7f9ea5
diptowebhero <diptodas1672@gmail.com>
2023-09-20 18:27:01 +0600

    feat: add banner mobile version

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,18 +4,18 @@ const Banner = () => {
   return (
     <>
       <div className="min-h-screen bg-[#C06F39;] flex items-center py-6 lg:py-6">
-        <div className="flex items-center justify-between w-full container flex-col space-y-12 lg:space-y-0 lg:flex-row md:flex-col md:space-y-12">
+        <div className="flex items-center justify-between w-full container flex-col-reverse space-y-12 lg:space-y-0 lg:flex-row md:flex-col-reverse md:space-y-12">
           {/* Banner left Area */}
           <div className="text-center">
             <h1 className="text-[#FFE88C] font-normal flex items-center justify-center flex-col">
-              <span className="text-[56px] leading-[69px] lg:text-[65px] lg:leading-[100px] md:text-[65px] md:leading-[100px]">
+              <span className="text-[30px] leading-[100px] md:text-[65px] md:leading-[100px]">
                 GET YOUR
               </span>
-              <span className="text-[56px] leading-[69px] lg:text-[75px] lg:leading-[100px] md:text-[75px] md:leading-[100px]">
+              <span className="text-[45px] leading-[55px] md:text-[75px] md:leading-[100px]">
                 TICKETS NOW
               </span>
             </h1>
-            <button className="mt-[43px] w-[265px] h-[78px] rounded-[39px] bg-[#A34411] text-white text-[26px] font-normal leading-[65px] text-center">
+            <button className="mt-[43px] max-w-[159px] mx-auto h-[43px] w-full rounded-[39px] bg-[#A34411] text-white text-[14px] font-normal leading-[65px] text-center flex items-center justify-center md:text-[26px] md:max-w-[265px] md:h-[78px]">
               Get Passes
             </button>
           </div>
