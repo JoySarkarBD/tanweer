49024d4c41884feb075bb8a0b189dc66f9f0278c
diptowebhero <diptodas1672@gmail.com>
2023-09-20 13:33:07 +0600

    feat: add banner section

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{}],
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
