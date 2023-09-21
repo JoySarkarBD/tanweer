e4a08873f710b0462f09d5ddccec8b50c4a69d89
arunabhnewar <newardevil@gmail.com>
2023-09-21 23:17:14 +0600

    fix: about desktop, laptop responsive done

--- src/components/AboutPrograms/AboutPrograms.jsx
+++ src/components/AboutPrograms/AboutPrograms.jsx
@@ -20,51 +20,56 @@ export default function AboutPrograms() {
   };
   return (
     <>
-      <div className="container pt-[80px] bg-[#C06F39] ">
-        <div className="tabs flex justify-center items-center">
+      <div className='container pt-[80px] bg-[#C06F39] '>
+        <div className='tabs flex justify-center items-center'>
           {tabs.map((tab, index) => (
             <div
               key={index}
               className={` tab ${
                 index === activeTab
-                  ? "active text-[#ffe88c] px-10 text-[32px] h-[93px] "
-                  : "px-10 text-[#e0aa51] text-[32px] h-[93px] "
+                  ? "active text-[#ffe88c] xl:px-10 lg:px-7 md:px-3 xl:text-[32px] lg:text-[28px] md:text-[24px] h-[93px]  "
+                  : " xl:px-10 lg:px-7 md:px-3 text-[#e0aa51] xl:text-[32px] lg:text-[28px] md:text-[24px] h-[93px]  "
               }`}
-              onClick={() => handleTabClick(index)}
-            >
+              onClick={() => handleTabClick(index)}>
               {tab}
             </div>
           ))}
         </div>
 
-        <div className="tab-content">
+        <div className='tab-content'>
           {tabs[activeTab] === "Festival Mission" && (
             <>
-              <div className="flex justify-around pt-[100px] ">
-                <div className="pt-[100px] w-[425px] h-[672px]">
-                  <img
-                    src={festiveImg}
-                    alt=""
-                    className="max-w-[425px] w-full "
-                  />
+              <div className='flex justify-around xl:pt-[100px] lg:pt-[80px] md:pt-[60px] '>
+                <div className='pt-[100px] xl:max-w-[425px] lg:max-w-[380px] md:max-w-[320px]  xl:h-[672px] lg:h-[630px] md:h-[580px] '>
+                  <img src={festiveImg} alt='' className=' w-full ' />
                 </div>
 
                 <div>
-                  <div className="w-[882px]">
-                    <h1 className="text-[#FFF7E0] text-[64px] font-medium leading-[90px] flex justify-end">
+                  <div className='xl:w-[882px] lg:w-[700px] md:w-[450px]   '>
+                    <h1 className='text-[#FFF7E0] 2xl:text-[64px] xl:text-[54px] lg:text-[44px] md:text-[30px] font-medium 2xl:leading-[90px] xl:leading-[80px] lg:leading-[70px] md:leading-[50px] flex justify-end'>
                       It is through music that we <br /> can become enlightened
-                       and<br /> connected to the divine.
+                      and
+                      <br /> connected to the divine.
                     </h1>
                   </div>
 
-                  <div className="text-[#FFF7E0] pt-[70px] w-full flex flex-col items-center">
-                    <p className="max-w-[631px] w-full text-[26px] pb-[26px]">
-                      This is why we are bringing together artists from around the world who understand our message, and the need to revive and bring back our ancient wisdom through sacred songs and traditionalmusic – music that will help us remember our past and revive our traditions. This is Tanweer.
-                    </p>
-
-                    <p className="max-w-[631px] w-full text-[26px]">
+                  <div className='text-[#FFF7E0] xl:pt-[70px] lg:pt-[55px] md:pt-[45px] xl:w-[631px] lg:w-[475px] md:w-[300px] flex flex-col  ms-auto'>
+                    <p className=' xl:text-[26px] lg:text-[22px] md:text-[18px] pb-[26px] flex justify-end'>
+                      This is why we are bringing together artists from around
+                      the world who understand our message, and the need to
+                      revive and bring back our ancient wisdom through sacred
+                      songs and traditional music – music that will help us
+                      remember our past and revive our traditions. This is
+                      Tanweer.
+                      <br />
+                      <br />
                       Tanweer means enlightenment – a meeting of minds and
-                      hearts. Tanweer will bring together the keepers of wisdom to raise our collective frequency and connect us with our past. Thank you for joining us. With love and gratitude,<span className="block pt-[22px]">Bodour Al Qasimi</span>  
+                      hearts. Tanweer will bring together the keepers of wisdom
+                      to raise our collective frequency and connect us with our
+                      past. Thank you for joining us. With love and gratitude,
+                      <br />
+                      <br />
+                      Bodour Al Qasimi
                     </p>
                   </div>
                 </div>
