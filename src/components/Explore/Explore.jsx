deab7b18a4e9a366f6945f98343e08aac54d3ee7
arunabhnewar <newardevil@gmail.com>
2023-09-23 19:41:14 +0600

    refactor: slider code refactor

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -1,44 +1,46 @@
 import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { EffectCoverflow, Navigation } from "swiper/modules";
+import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import extraImg from "../../assets/sliderImg.png";
 import "./Explore.css";
 
-import {
-  default as sliderImg1,
-  default as sliderImg2,
-  default as sliderImg3,
-  default as sliderImg4,
-  default as sliderImg5,
-  default as sliderImg6,
-} from "../../assets/sliderImg.png";
-
-const exploresSlide = [
+const slidersData = [
   {
     id: 1,
-    img: sliderImg1,
+    img: extraImg,
+    caption:
+      "Tanweer Sacred Music Festival will feature workshops and sessions on music, vibration, and singing led by experienced medicine musicians, teachers, and facilitators.",
   },
   {
     id: 2,
-    img: sliderImg2,
+    img: extraImg,
+    caption:
+      "Tanweer Sacred Music Festival will feature workshops and sessions on music, vibration, and singing led by experienced medicine musicians, teachers, and facilitators.",
   },
   {
     id: 3,
-    img: sliderImg3,
+    img: extraImg,
+    caption:
+      "Tanweer Sacred Music Festival will feature workshops and sessions on music, vibration, and singing led by experienced medicine musicians, teachers, and facilitators.",
   },
   {
     id: 4,
-    img: sliderImg4,
+    img: extraImg,
+    caption:
+      "Tanweer Sacred Music Festival will feature workshops and sessions on music, vibration, and singing led by experienced medicine musicians, teachers, and facilitators.",
   },
   {
     id: 5,
-    img: sliderImg5,
+    img: extraImg,
+    caption:
+      "Tanweer Sacred Music Festival will feature workshops and sessions on music, vibration, and singing led by experienced medicine musicians, teachers, and facilitators.",
   },
   {
     id: 6,
-    img: sliderImg6,
+    img: extraImg,
+    caption:
+      "Tanweer Sacred Music Festival will feature workshops and sessions on music, vibration, and singing led by experienced medicine musicians, teachers, and facilitators.",
   },
 ];
 
@@ -47,80 +49,34 @@ export default function Explore() {
     <>
       <div className='bg-exploreBg bg-no-repeat bg-cover bg-center py-20'>
         <div>
-          <h1 className='text-[#FFF7E0] text-center  md:text-[60px] xsm:text-[40px] font-normal  '>
+          <h1 className='text-[#FFF7E0] text-center  lg:text-[80px]  md:text-[60px] sm:text-[50px]  font-normal  '>
             Explore Tanweer
           </h1>
         </div>
 
-        <div>
-          <Swiper
-            initialSlide={Math.floor(exploresSlide.length / 2)}
-            effect={"coverflow"}
-            grabCursor={true}
-            centeredSlides={true}
-            slidesPerView={"auto"}
-            loop={true}
-            coverflowEffect={{
-              rotate: 50,
-              stretch: 0,
-              depth: 100,
-              modifier: 1,
-              slideShadows: true,
-            }}
-            navigation={true}
-            modules={[EffectCoverflow, Navigation]}
-            breakpoints={{
-              481: {
-                slidesPerView: 1,
-                spaceBetween: 2,
-              },
-              640: {
-                slidesPerView: 1,
-                spaceBetween: 2,
-              },
-              641: {
-                slidesPerView: 3,
-                spaceBetween: 2,
-              },
-              768: {
-                slidesPerView: 3,
-                spaceBetween: 10,
-              },
-              1024: {
-                slidesPerView: 3,
-                spaceBetween: 10,
-              },
-              1280: {
-                slidesPerView: 3,
-                spaceBetween: 10,
-              },
-              1536: {
-                slidesPerView: 3,
-                spaceBetween: 10,
-              },
-            }}>
-            {exploresSlide.map((data) => {
-              return (
-                <SwiperSlide key={data.id}>
-                  <div key={data.id} data={data}>
-                    <img src={data.img} alt='' />
-                  </div>
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
-        </div>
-
-        <div className='lg:w-[600px] md:w-[500px] xmd:w-[450px]  mx-auto mt-[-40px]'>
-          <p className='text-center text-[#FFF7E0] md:text-[18px] xmd:text-[16px] font-normal '>
-            Tanweer Sacred Music Festival will feature workshops and <br />{" "}
-            sessions on music, vibration, and singing led by experienced <br />{" "}
-            medicine musicians, teachers, and facilitators.
-          </p>
-        </div>
+        <Swiper
+          slidesPerView={"auto"}
+          centeredSlides={true}
+          spaceBetween={30}
+          modules={[Navigation]}
+          loop={true}
+          navigation>
+          {slidersData.map((data) => {
+            return (
+              <SwiperSlide key={data.id}>
+                <div key={data.id} data={data} className='flex flex-wrap'>
+                  <img src={data.img} alt='' />
+                  <p className='caption lg:pt-[50px] md:pt-[35px] xmd:pt-[25px] sm:pt-[20px] xsm:pt-[15px] text-[#FFF7E0] text-[12px] leading-4 sm:text-[16px] sm:leading-5 md:text-[20px] md:leading-6 lg:text-[24px] lg:leading-8  '>
+                    {data.caption}
+                  </p>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
 
         <div className='text-center pt-[81px] pb-[13px]  '>
-          <button className='text-[#FFF7E0] text-[22px] font-normal bg-[#A34411] rounded-[35px] px-8 py-4'>
+          <button className='text-[#FFF7E0] lg:text-[22px] md:text-[20px] sm:text-[16px] font-normal bg-[#A34411] rounded-[35px] px-8 py-4'>
             Explore Wellbeing at Tanweer
           </button>
         </div>
